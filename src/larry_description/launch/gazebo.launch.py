426e4554a00d0a39729a3de08f96c6fe21e691ce
Nathan Wanjongkhum <wanjongkhumnathan@gmail.com>
2026-03-03 18:52:10 -0800

    temp gz

---
+++ src/larry_description/launch/gazebo.launch.py
@@ -0,0 +1,49 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+# ros2 run ros_gz_bridge parameter_bridge /cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist
+
+def generate_launch_description():
+    pkg_path = get_package_share_directory('larry_description')
+
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(pkg_path, 'launch', 'rsp.launch.py')]),
+        launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+        launch_arguments={'gz_args': '-r empty.sdf'}.items()
+    )
+
+    spawn_robot = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=['-topic', 'robot_description', '-name', 'larry_robot'],
+        output='screen'
+    )
+
+    load_joint_state_broadcaster = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+    )
+
+    load_diff_drive_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_drive_controller"],
+    )
+
+    return LaunchDescription([
+        rsp, 
+        gazebo, 
+        spawn_robot,
+        load_joint_state_broadcaster,
+        load_diff_drive_controller 
+    ])
---
+++ src/larry_description/urdf/params.md
@@ -0,0 +1,15 @@
+# Simulation Parameters
+
+## Wheel Inertia
+
+- Radius (r): 60mm = 0.06m
+- Length (l): 40mm = 0.04m
+- Mass (m): 0.5kg per wheel.
+
+$$I_{xx} = I_{yy} = \frac{1}{12}ml^2 + \frac{1}{4}mr^2$$
+
+$$I_{xx} = I_{yy} = \frac{1}{12}(0.5)(0.04)^2 + \frac{1}{4}(0.5)(0.06)^2 = 0.000516$$
+
+$$I_{zz} = \frac{1}{2}mr^2$$
+
+$$I_{zz} = \frac{1}{2}(0.5)(0.06)^2 = 0.0009$$
