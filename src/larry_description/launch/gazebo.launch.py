e5dba986d433119d46bbeb23b97100f5dc7757f2
Nathan Wanjongkhum <wanjongkhumnathan@gmail.com>
2026-03-03 22:06:58 -0800

    Schedlued nodes

--- src/larry_description/launch/gazebo.launch.py
+++ src/larry_description/launch/gazebo.launch.py
@@ -1,11 +1,10 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, AppendEnvironmentVariable
+from launch.actions import IncludeLaunchDescription, AppendEnvironmentVariable, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-
-# ros2 run ros_gz_bridge parameter_bridge /cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist
 
 def generate_launch_description():
     pkg_path = get_package_share_directory('larry_description')
@@ -33,16 +32,44 @@ def generate_launch_description():
         output='screen'
     )
 
+    # Clock bridge for simulation time synchronization
+    clock_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'],
+        output='screen'
+    )
+
     load_joint_state_broadcaster = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["joint_state_broadcaster"],
     )
 
+    # Remap the controller's command topic to match teleop_twist_keyboard
     load_diff_drive_controller = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["diff_drive_controller"],
+        arguments=[
+            "diff_drive_controller",
+            "--controller-ros-args",
+            "-r", "/diff_drive_controller/cmd_vel:=/cmd_vel"
+        ],
+    )
+
+    # Sequence the startups using Event Handlers
+    delay_jsb_after_spawn = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=spawn_robot,
+            on_exit=[load_joint_state_broadcaster],
+        )
+    )
+
+    delay_ddc_after_jsb = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=load_joint_state_broadcaster,
+            on_exit=[load_diff_drive_controller],
+        )
     )
 
     return LaunchDescription([
@@ -50,6 +77,7 @@ def generate_launch_description():
         rsp, 
         gazebo, 
         spawn_robot,
-        load_joint_state_broadcaster,
-        load_diff_drive_controller 
+        clock_bridge,
+        delay_jsb_after_spawn,
+        delay_ddc_after_jsb 
     ])
